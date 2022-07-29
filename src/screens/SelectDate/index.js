1cffac704a8e252f99072671045db85fdeae4744
AlexsanderSoares <alexsandersoares30@gmail.com>
2022-07-29 20:22:44 -0300

    Configurando calandar picker na screen SelectDate

--- src/screens/SelectDate/index.js
+++ src/screens/SelectDate/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import CalendarPicker from 'react-native-calendar-picker';
 
 import { Container } from './styles';
 
-const SelectDate = () => {
-  return (
+const SelectDate = (props) => {
+
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    return (
         <Container>
-            
+            <CalendarPicker onDateChange={setSelectedDate}/>
         </Container>
-  );
+    );
 }
 
 export default SelectDate;
