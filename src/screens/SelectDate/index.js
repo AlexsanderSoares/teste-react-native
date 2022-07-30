5cc01dc7322104645223a3e76d6680d5545107c1
AlexsanderSoares <alexsandersoares30@gmail.com>
2022-07-29 22:59:38 -0300

    Configurando context na screen FinishScheduling

--- src/screens/SelectDate/index.js
+++ src/screens/SelectDate/index.js
@@ -1,13 +1,24 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import CalendarPicker from 'react-native-calendar-picker';
 import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
+import {Text} from 'react-native';
 
 import { Container, SelectDateTitleContainer, SelectDateTitle, 
                                 SelectDateSubtitle, ButtonNextConatiner, ButtonNext, ButtonNextText } from './styles';
 
+import {SchedulingContext} from '../../contexts/scheduling';
+
 const SelectDate = (props) => {
 
-    const [selectedDate, setSelectedDate] = useState(null);
+    const [selectedDate, setSelectedDate] = useState("");
+
+    const {saveDateScheduling} = useContext(SchedulingContext);
+
+    function handleNext(){
+        saveDateScheduling(selectedDate.toJSON());
+
+        props.navigation.navigate("FinishScheduling");
+    }
 
     return (
         <Container>
