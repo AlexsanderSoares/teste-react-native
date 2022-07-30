8a21372622862871dae2a2320e5bc7ea08b65ba7
AlexsanderSoares <alexsandersoares30@gmail.com>
2022-07-30 11:52:33 -0300

    Criando componente RowList

---
+++ src/components/RowList/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { RowContainer, Label, Info } from './styles';
+
+const RowList = (props) => {
+  return (
+        <RowContainer>
+            <Label>
+                {props.label}
+            </Label>
+            <Info>
+                {props.info}
+            </Info>
+        </RowContainer>
+  );
+}
+
+export default RowList;
---
+++ src/components/RowList/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components/native";
+
+export const RowContainer = styled.View`
+    flex-direction: row;
+    border-width:1px;
+    border-color: #eee;
+    margin-top: 0px;
+    padding: 12px;
+    background-color: #fff;
+`;
+
+export const Label = styled.Text`
+    font-size: 18px;
+    font-weight: bold;
+    color: #555;
+    margin-right: 5px;
+`;
+
+export const Info = styled.Text`
+    font-size: 18px;
+    color: #555;
+`;
