546758674e37cc58dab58aabfd823b72d8493aec
AlexsanderSoares <alexsandersoares30@gmail.com>
2022-07-29 21:36:22 -0300

    Layout de screen finalizar atendimento finalizada

--- src/screens/FinishScheduling/index.js
+++ src/screens/FinishScheduling/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
-
-import { Container, FinishSchedulingTitleContainer, FinishSchedulingTitle, FinishSchedulingSubtitle } from './styles';
+import { Container, FinishSchedulingTitleContainer, 
+                FinishSchedulingTitle, FinishSchedulingSubtitle, 
+                        RowContainer, InfoContainer, Label, Info,
+                                FinishButtonContainer, FinishButton, FinishButtonText } from './styles';
+import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const FinishScheduling = () => {
     return (
@@ -11,9 +13,66 @@ const FinishScheduling = () => {
                     Confira os dados e confirme
                 </FinishSchedulingTitle>
                 <FinishSchedulingSubtitle>
-                    Confira se os dados abaixo, caso estejam corretos confirme o agendamento para finalizar.
+                    Confira se os dados abaixo, caso estejam corretos confirme para finalizar o agendamento.
                 </FinishSchedulingSubtitle>
             </FinishSchedulingTitleContainer>
+            <InfoContainer>
+                <RowContainer>
+                    <Label>
+                        Nome: 
+                    </Label>
+                    <Info>
+                        Alexsander
+                    </Info>
+                </RowContainer>
+                <RowContainer>
+                    <Label>
+                        CPF: 
+                    </Label>
+                    <Info>
+                        076.522.283-36
+                    </Info>
+                </RowContainer>
+                <RowContainer>
+                    <Label>
+                        Data de nascimento: 
+                    </Label>
+                    <Info>
+                        19/11/1999
+                    </Info>
+                </RowContainer>
+                <RowContainer>
+                    <Label>
+                        Telefone: 
+                    </Label>
+                    <Info>
+                        (86) 99557-7042
+                    </Info>
+                </RowContainer>
+                <RowContainer>
+                    <Label>
+                        Especialidade médica: 
+                    </Label>
+                    <Info>
+                        Cardiologista
+                    </Info>
+                </RowContainer>
+                <RowContainer>
+                    <Label>
+                        Data para agendamento: 
+                    </Label>
+                    <Info>
+                        28/07/2022
+                    </Info>
+                </RowContainer>
+            </InfoContainer>
+            <FinishButtonContainer>
+                <FinishButton>
+                    <FinishButtonText>
+                        Confirmar agendamento
+                    </FinishButtonText>
+                </FinishButton>
+            </FinishButtonContainer>
         </Container>
     );
 }
--- src/screens/FinishScheduling/styles.js
+++ src/screens/FinishScheduling/styles.js
@@ -30,3 +30,54 @@ export const FinishSchedulingSubtitle = styled.Text`
     color: #fff;
     /* font-weight: bold; */
 `;
+
+export const InfoContainer = styled.View`
+    width: 100%;
+    margin-top: 10px;
+`;
+
+export const RowContainer = styled.View`
+    flex-direction: row;
+    border-width:1px;
+    border-color: #eee;
+    margin-top: 0px;
+    padding: 12px;
+    background-color: #fff;
+`;
+
+export const Label = styled.Text`
+    font-size: 18px;
+    font-weight: bold;
+    color: #555;
+    margin-right: 5px;
+`;
+
+export const Info = styled.Text`
+    font-size: 18px;
+    color: #555;
+`;
+
+export const FinishButtonContainer = styled.View`
+    align-items: center;
+    justify-content: center;
+    align-content: center;
+    margin-top: 50px;
+`;
+
+export const FinishButton = styled.TouchableOpacity`
+    width: 200px;
+    height: 60px;
+    border-radius: 20px;
+    border: none;
+    align-items: center;
+    justify-content: center;
+    align-content: center;
+    background-color: #fff;
+`;
+
+export const FinishButtonText = styled.Text`
+    font-size: 20px;
+    color: #777;
+    font-weight: bold;
+    text-align: center;
+`;
