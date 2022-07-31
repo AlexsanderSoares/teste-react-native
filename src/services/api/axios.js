a6267187d518533f8a70643ef28ae3d8902fbfeb
AlexsanderSoares <alexsandersoares30@gmail.com>
2022-07-31 00:44:39 -0300

    COnfigurando variaveis de ambiente na api

--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import {API_URL} from '@env';
 
 const api = axios.create({
-    baseURL: 'https://webhook.site/b8ee07df-4aaa-4601-9d7c-0342810873ab',
+    baseURL: API_URL,
 });
 
 export default api;
